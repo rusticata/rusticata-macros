e18a03383c419f1e56c42d1e214e0e34bcabf6b4
Pierre Chifflier <chifflier@wzdftpd.net>
2019-12-03 16:29:40 +0100

    Add combinator versions for align32, cond_else and pure (ex-value)

--- src/combinator.rs
+++ src/combinator.rs
@@ -5,6 +5,14 @@ use nom::error::ParseError;
 use nom::{IResult, InputLength, ToUsize};
 use nom::{InputIter, InputTake};
 
+/// Create a combinator that returns the provided value, and input unchanged
+pub fn pure<I, O, E: ParseError<I>>(val: O) -> impl Fn(I) -> IResult<I, O, E>
+where
+    O: Clone,
+{
+    move |input: I| Ok((input, val.clone()))
+}
+
 /// Return a closure that takes `len` bytes from input, and applies `parser`.
 pub fn flat_take<I, C, O, E: ParseError<I>, F>(len: C, parser: F) -> impl Fn(I) -> IResult<I, O, E>
 where
@@ -35,11 +43,42 @@ where
     flat_take(len, parser)(input)
 }
 
+/// Helper macro for nom parsers: run first parser if condition is true, else second parser
+pub fn cond_else<I: Clone, O, E: ParseError<I>, C, F, G>(
+    cond: C,
+    first: F,
+    second: G,
+) -> impl Fn(I) -> IResult<I, O, E>
+where
+    C: Fn() -> bool,
+    F: Fn(I) -> IResult<I, O, E>,
+    G: Fn(I) -> IResult<I, O, E>,
+{
+    move |input: I| {
+        if cond() {
+            first(input)
+        } else {
+            second(input)
+        }
+    }
+}
+
+/// Align input value to the next multiple of n bytes
+/// Valid only if n is a power of 2
+pub const fn align_n2(x: usize, n: usize) -> usize {
+    (x + (n - 1)) & !(n - 1)
+}
+
+/// Align input value to the next multiple of 4 bytes
+pub const fn align32(x: usize) -> usize {
+    (x + 3) & !3
+}
+
 #[cfg(test)]
 mod tests {
-    use super::flat_take;
+    use super::{align32, cond_else, flat_take, pure};
     use nom::bytes::streaming::take;
-    use nom::number::streaming::{be_u16, be_u32};
+    use nom::number::streaming::{be_u16, be_u32, be_u8};
     use nom::{Err, IResult, Needed};
 
     #[test]
@@ -68,5 +107,41 @@ mod tests {
         // read 2 bytes and a use combinator requiring more bytes
         let res: IResult<&str, &str> = flat_take(2u8, take(4u8))(input);
         assert_eq!(res, Err(Err::Incomplete(Needed::Size(4))));
+    }
+
+    #[test]
+    fn test_cond_else() {
+        let input = &[0x01][..];
+        let empty = &b""[..];
+        let a = 1;
+        fn parse_u8(i: &[u8]) -> IResult<&[u8], u8> {
+            be_u8(i)
+        }
+        assert_eq!(
+            cond_else(|| a == 1, parse_u8, pure(0x02))(input),
+            Ok((empty, 0x01))
+        );
+        assert_eq!(
+            cond_else(|| a == 1, parse_u8, pure(0x02))(input),
+            Ok((empty, 0x01))
+        );
+        assert_eq!(
+            cond_else(|| a == 2, parse_u8, pure(0x02))(input),
+            Ok((input, 0x02))
+        );
+        assert_eq!(
+            cond_else(|| a == 1, pure(0x02), parse_u8)(input),
+            Ok((input, 0x02))
+        );
+        let res: IResult<&[u8], u8> = cond_else(|| a == 1, parse_u8, parse_u8)(input);
+        assert_eq!(res, Ok((empty, 0x01)));
+    }
+
+    #[test]
+    fn test_align32() {
+        assert_eq!(align32(3), 4);
+        assert_eq!(align32(4), 4);
+        assert_eq!(align32(5), 8);
+        assert_eq!(align32(5usize), 8);
     }
 }
